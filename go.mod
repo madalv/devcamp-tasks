9bb89ee1f35dcbb90971267a646ac5f3126419f1
madalv <vlada.magal@isa.utm.md>
2024-04-02 18:58:40 +0300

    tables creation script + data generation

---
+++ go.mod
@@ -0,0 +1,23 @@
+module adt
+
+go 1.22
+
+toolchain go1.22.1
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/gookit/slog v0.5.5
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/brianvoe/gofakeit/v7 v7.0.2 
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/gookit/goutil v0.6.15 // indirect
+	github.com/gookit/gsr v0.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ queries.sql
@@ -0,0 +1,34 @@
+
+create table campaigns (
+  id bigint unsigned auto_increment primary key,
+  name varchar(255) not null
+)
+
+create table sources (
+    id bigint unsigned auto_increment primary key, 
+    name varchar(255) not null
+)
+
+create table campaigns_sources (
+  campaign_id bigint unsigned not null,
+  source_id bigint unsigned not null,
+  foreign key (campaign_id) references campaigns(id),
+  foreign key (source_id) references sources(id)
+)
+
+-- ensure that a source can be used in a maximum of 10 campaigns
+create trigger trg_chk_campaign_limit
+before insert on campaigns_sources
+for each row
+begin
+  set @cnt = (select count(*) from campaigns_sources where source_id = new.source_id);
+  
+  if @cnt >= 10 then
+    signal sqlstate "45000" set message_text = "Campaigns limit reached";
+  end if;
+end
+
+-- Queries
+
+
+
